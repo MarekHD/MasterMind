85148a1f4536a8771c43653c68d9718b25ca89ed
MarekHD <szymczykmail@gmail.com>
2017-10-30 00:25:16 +0100

    Proby na finalDivision

--- js/Components/AnswerWraper.jsx
+++ js/Components/AnswerWraper.jsx
@@ -6,17 +6,37 @@ class AnswerWraper extends React.Component {
     }
 
     componentDidMount(){
-        let hint; // to co bedzie przekazywane do className po porownaniu indexu z codem
-
+        
     }
 
-
-
-
     render() {
+        //mecze finalDivison
+        finalDivision = (element)=>{
+            arrayAnswerFilter = guessPigCorrect.filter((element)=>{
+                element === this.
+            })
+
+        }
+        
+        let greenCounter = 0
+        let semiCounter = 0
+        console.log('TU PRZYCHODZI KOD DO SPRAWDZENIA',this.props.code)
+        let guessPig = this.props.index.slice();
+        console.log('HINT',guessPig) // to co bedzie przekazywane do className po porownaniu indexu z codem
+        let guessPigCorrect = guessPig.map((elem, i)=>{
+            if (elem === this.props.code[i]) {
+                greenCounter++;
+                return 'dot-correct'
+            } else return elem //pozniej zrobie z tego kolor, zeby przepuszczal do kolejnego sprawdzania
+        })
+        //Nie wiem czy tu ma być ten finalDivision
+        let gPcorrectPlusSemi = guessPigCorrect.map((elem, i)=>{
+            return finalDivision(elem)
+        })
+
         const dots = [0,1,2,3];
         const dotsList = dots.map((elem, i)=>{
-            return <div key={i} order={elem} className="dot "></div>
+            return <div key={i} order={elem} className={theSky[i]}></div>
         })
         return  <div className="answer-wraper">
                     {dotsList}
