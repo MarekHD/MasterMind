0529cdaf9e3f6374b2ca1e1059295ef48fdfad78
MarekHD <szymczykmail@gmail.com>
2017-11-02 13:33:04 +0100

    Wyglad do poprawy. Ustawienie EndGame (win and lose).

--- js/Components/ColorsTitle.jsx
+++ js/Components/ColorsTitle.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 class ColorsTitle extends React.Component {
     render() {
         return  <div onClick={this.props.resetRow} className="color-title">
-                    <h2>Choose Color:</h2>
+                    <h2>Click on color to choose:</h2>
                 </div>
     }
 }
--- js/Components/Rules.jsx
+++ js/Components/Rules.jsx
@@ -5,7 +5,9 @@ import React from 'react';
 class Rules extends React.Component {
     render() {
         return  <div className="rules">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione perferendis cupiditate quos dolorem dignissimos. Animi unde, quis delectus, voluptatibus molestias iusto culpa ut est quos nam consequatur aliquam voluptatum rem?
+                    <h2>RULES</h2>
+                    <br/>
+                    <span>Try to guess the circle pattern, in both order and color, within 10 turns. After submitting a row, a small green peg is placed for each code peg from the guess which is correct in both color and position. A lightblue peg indicates the existence of a correct color code peg placed in the wrong position. More info on <a target="blank" href="https://en.wikipedia.org/wiki/Mastermind_(board_game)">Wikipedia</a>.</span>
                 </div>
     }
 }
